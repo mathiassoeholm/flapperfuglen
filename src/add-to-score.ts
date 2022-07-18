65a33987d5e19ff21fdf93f718517e734b0faf6d
Mathias Soeholm <mathiassoeholm@gmail.com>
2022-07-18 21:45:31 +0200

    wip

--- src/add-to-score.ts
+++ src/add-to-score.ts
@@ -16,25 +16,36 @@ interface EntityTypes {
   curve: null;
 }
 
+type This<
+  TFields extends {
+    [key: string]: any;
+  },
+  TAttributes extends {
+    [key: string]: EntityArgs;
+  }
+> = pc.ScriptType & {
+  [K in keyof TAttributes]: EntityTypes[TAttributes[K]["type"]];
+} & TFields;
+
 const createScript = <
+  TFields extends {
+    [key: string]: any;
+  },
   TAttributes extends {
     [key: string]: EntityArgs;
   }
 >(
   name: string,
   {
+    fields,
     attributes,
     initialize,
     update,
   }: {
+    fields?: TFields;
     attributes?: TAttributes;
-    initialize?: (this: pc.ScriptType) => void;
-    update?: (
-      this: pc.ScriptType & {
-        [K in keyof TAttributes]: EntityTypes[TAttributes[K]["type"]];
-      },
-      dt: number
-    ) => void;
+    initialize?: (this: This<TFields, TAttributes>) => void;
+    update?: (this: This<TFields, TAttributes>, dt: number) => void;
   } = {}
 ) => {
   var Script = pc.createScript(name)!;
@@ -45,33 +56,40 @@ const createScript = <
     );
   }
 
-  Script.prototype.initialize = function () {
+  Script.prototype.initialize = function (this: This<TFields, TAttributes>) {
+    if (fields) {
+      Object.keys(fields).forEach(
+        (f: keyof TFields) => ((this as any)[f] = fields[f])
+      );
+    }
     initialize?.call(this);
   };
 
-  Script.prototype.update = function (dt) {
+  Script.prototype.update = function (this: This<TFields, TAttributes>, dt) {
     update?.call(this, dt);
   };
 };
 
-let lastX = 0;
-
 createScript("addToScore", {
+  fields: {
+    lastX: 0,
+  },
   attributes: {
     bird: { type: "entity" },
     pos: { type: "vec4" },
   },
   initialize() {
-    lastX = this.entity.getPosition().x;
+    this.lastX = this.entity.getPosition().x;
+    console.log(this.pos);
   },
   update() {
     const app = this.app;
     const birdX = this.bird.getPosition().x;
     const pipeX = this.entity.getPosition().x;
 
-    if (pipeX <= birdX && lastX > birdX) {
+    if (pipeX <= birdX && this.lastX > birdX) {
       app.fire("game:addscore");
     }
-    lastX = pipeX;
+    this.lastX = pipeX;
   },
 });
